089adabc85e71f8b952a88b8be90c5f2d35759c7
matthewbillienyc <matthewbillienyc@gmail.com>
2015-09-03 18:09:34 -0400

    passed

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
+    keys = []
+    args = arguments
     # code goes here
+    self.each do |key, value|
+      if args.include?(value)
+        keys << key
+      end
+    end
+    keys
   end
 end
