7b4c7125be1e2c32750d525df8a2ed2d3182f1e4
matthewmain <matthewwmain@gmail.com>
2015-09-26 10:30:45 -0400

    monkey-patched Hash with splat method

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*locations)
+    animals_arr = []
+    locations.each { | location | 
+    	self.each { | animal, home |	    	
+    		if location == home
+	    		animals_arr << animal
+	    	end
+    	}
+   	}
+    animals_arr
   end
 end
