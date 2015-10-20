62333784e37a7b8d4e574d6d09c7bc29dff6e289
wifelette <leahsilber@gmail.com>
2015-10-20 00:21:47 -0400

    Passing

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    keys = []
+    arguments.each do |given_value|
+      self.each do |key, value|
+        if value == given_value
+          keys << key
+        end
+      end
+    end
+    keys
   end
 end
