db5e8082cb63049000835a1dea5d7246c2898cab
flats <dave@robador.com>
2015-10-26 03:07:10 -0400

    first commit - keys of hash method

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    matches = []
+    arguments.each do |arg|
+      self.each do |k, v|
+        if arg == v then matches << k end
+      end
+    end
+    matches
   end
 end
