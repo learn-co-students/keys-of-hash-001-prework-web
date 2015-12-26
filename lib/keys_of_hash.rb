de15d81b2329ee1b0532788ac370e3c83176b69c
chintas1 <chintas1@tcnj.edu>
2015-12-26 17:42:24 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    matches = []
+    arguments.each do |search|
+      self.each do |key, value|
+        matches << key if value == search
+      end
+    end
+    matches
   end
 end
