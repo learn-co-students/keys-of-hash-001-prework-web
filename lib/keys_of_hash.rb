c33ca89cfcd6ee7a06ea4cde6956180b2705a3fc
SaiedAbbasi <saiedabbasijr@gmail.com>
2016-01-30 11:53:01 -0500

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    a2 =[]
+    self.each do |keys, values|
+      arguments.each do |animal|
+        if values == animal
+        #plugged in (if values == arguments[0]) zero to pull out a single argument... 6/7 tests passing??? no idea
+        #not completely clear what is happening... more specifically what the hash looks like, but passing
+        a2 << keys
+        end
+      end
+    end
+    a2
+
   end
 end
