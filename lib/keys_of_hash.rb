a16244a56b7a2d2aaea7fb0318de4d72c9d9049c
kazushige Miyao <yura.soverigns@gmail.com>
2016-02-25 12:14:09 +0900

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,18 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    key_name = []
+    arguments.each do |arg|
+		self.each do |self_key, value| #self_key ="sugar glider",value "Australia"
+			if value == arg
+				# #binding.pry
+				key_name.push(self_key)
+				#binding.pry
+			end
+		end
+	#binding.pry
+	end
+  	key_name
   end
 end
