35cbde627a4fadf04e0ee7489404ff5aa88fb843
samuelstuckey <sam.n.stuckey@gmail.com>
2016-02-25 14:14:11 -0500

    done

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+    arguments.each do |argument|
+      self.each do |key, value| # <-  how do i referece the hash I'm checking when it's not being passed as an argument?
+        array << key if value == argument
+      end
+    end
+    array
   end
 end
