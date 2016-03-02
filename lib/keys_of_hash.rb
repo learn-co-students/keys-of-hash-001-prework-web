3196882c07842e9a941f444ec46c791c43dd5cb0
jd2rogers2 <jd2rogers2@gmail.com>
2016-03-02 07:52:39 -0500

    donzo

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+    self.each do |animal, location|
+      if arguments.include?(location)
+        array << animal
+      end
+    end
+    array
   end
 end
