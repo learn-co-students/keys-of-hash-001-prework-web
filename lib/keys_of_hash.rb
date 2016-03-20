a89b73d44fd76f93f0e1bd152a7c816ef86caf8c
wengong <wentoshare@gmail.com>
2016-03-20 05:47:49 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    xyz=Array.new
+    self.collect do |i,g|
+      arguments.collect do |z|
+        if z.class == Array
+          if g==z.join or g==z.join.to_i
+            xyz << i
+          end
+        elsif g==z
+          xyz << i
+        end
+      end
+    end
+    return xyz
   end
 end
