d3750c4e4da4a7173d0cf0b72297c3c5f63798ac
bensu1013 <bensu1013@hotmail.com>
2016-03-26 04:17:01 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(arg, *args)
+    values = [arg, *args]
+    values.flatten!
+    array = Array.new
+    self.each {|key, val|
+      array.push(key) if values.include?(val)
+      }
+    array
   end
 end
