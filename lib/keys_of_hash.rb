16bb220e48f27dc4f787f219aa15d15c2a8232e0
Gordon Gao <gordonsdev@gmail.com>
2016-05-07 22:09:14 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    keys_arr = []
+    self.each { |k, v| keys_arr << k if arguments.include?(v) }
+    keys_arr
+
   end
 end
+
+# hash = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
+# hash.keys_of('Madagascar')
