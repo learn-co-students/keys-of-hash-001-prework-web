e0f371a879ea1a66bb0a9ce32f1031dedc2cd649
Fidel Severino <fidelscodes@gmail.com>
2016-05-26 09:49:48 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,20 @@
+# require "pry"
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    # self.map do |key,value|
+    #   # how do I make this part better? Not hardcoding for two parameters only
+    #   if value == arguments[0] || value == arguments[1]
+    #     key
+    #   end
+    # end.compact
+
+    keys_array = []
+
+    arguments.map do |arg|
+      self.each { |k,v| keys_array << k if arg == v }
+    end
+
+    keys_array
   end
 end
