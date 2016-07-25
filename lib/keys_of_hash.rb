a1a581f84233044426479ecf5226b799cb98bb74
mo6709 <mo6709@gmail.com>
2016-07-25 17:39:01 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    keys = []
+    arguments.each{|new_value|
+        self.each{|old_key, old_value|
+          if old_value == new_value
+          	keys << old_key
+          end
+        }
+    }
+    keys
   end
 end
