87ea55bcd34b06b950ce68a4158fc33b38451283
Brie Coyle <brie.coyle@gmail.com>
2016-08-06 21:33:59 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,6 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    new_array = []
+    new_array = collect {|k, v| arguments.include?(v) ? k : nil}.compact!
   end
 end
