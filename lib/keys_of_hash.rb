f31de44dccef9627d490789b0f782b031a7c5fce
Steven Tang <5teven7ang@gmail.com>
2016-08-07 09:11:23 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+  def keys_of(*arguments)
+		arguments_array = arguments.collect {|k| k}
+		collective_results_hash = Hash.new
+		i = 0
+
+		while i < arguments_array.length do
+			collective_results_hash.merge!(self.select {|k,v| v == arguments_array[i]})
+			i += 1
+		end
+		collective_results_hash.keys
+	end
 end
