545666617d0c7bf74e33f09461fb6582e90f7b95
Robert Hitt <robhitt@gmail.com>
2016-08-26 00:13:51 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,23 @@
+
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+
+  def keys_of(*argument)
+
+  key_array = []
+
+    self.each do |type_of_animal, country|
+
+      argument.each do |country_of_animal|
+
+          if country == country_of_animal
+
+            key_array.push(type_of_animal)
+
+          end
+      end
+    end
+
+    return key_array
   end
 end
